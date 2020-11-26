97e24ac40d512937f9ee80f70a02df6eb6efd344
LidaHak <61308358+LidaHak@users.noreply.github.com>
2020-11-26 16:26:11 +0400

    add, remove(first, last, specified), reverse, size

--- Linked_List.py
+++ Linked_List.py
@@ -6,12 +6,13 @@ class Node:
         self.size = None
 
 
-class Linked_List:
-    def __init__(self):
-        self.head = ""
-        self.tail = ""
 
-    # prints the values
+class Linked_List:
+    def __init__(self, value):
+        self.value = value
+        self.head = None
+        self.temp = None
+
     def display(self):
         values = self.head
         if values is None:
@@ -19,7 +20,7 @@ class Linked_List:
             return False
 
         while values:
-            print(str(values.data))
+            print(values.data, end=" -> ")
             values = values.next
 
     def size(self):
@@ -30,7 +31,6 @@ class Linked_List:
             current = current.next
         return length
 
-    # adds values to our list
     def add(self, data):
         new_node = Node(data, node='')
         if self.head is None:
@@ -41,18 +41,35 @@ class Linked_List:
             the_last_one = the_last_one.next
         the_last_one.next = new_node
 
-    def remove_first(self):
-        if self.head is not None:
+    def remove_from_beginning(self):
+        if self.head == None:
+            print('The list is empty!')
+        if self.head == None:
+            return None
+        else:
             self.head = self.head.next
 
-    def remove(self, data):
-        if self.head.data == data:
-            self.head = self.head.next
-        else:
-            node = self.head
-            while node.next:
-                if node.next.data == data:
-                    node.next = node.next.next
+    def remove_from_end(self):
+        if self.head == None:
+            return None
+        if self.head.next == None:
+            return None
+        temp = self.head
+        while (temp.next.next):
+            temp = temp.next
+        temp.next = None
+        return self.head
+
+    def remove_from_specific_position(self):
+        position = int(input("Please, enter the position you want to delete: "))
+        i = 1
+        temp = self.head
+        while i < position - 1:
+            temp = temp.next
+            i += 1
+        self.next = temp.next
+        temp.next = self.next.next
+        self.next = None
 
     def reverse(self):
         prev = None
@@ -61,20 +78,22 @@ class Linked_List:
             next = current.next
             current.next = prev
             prev = current
-            current = next
+            current = next  # the 'next' in this line is just a local variable
         self.head = prev
 
 
-our_list = Linked_List()
+
+our_list = Linked_List(value='')
 our_list.head = Node(1, node=0)
-#our_list.tail = Node(9, node=0)
 our_list.add(6)
 our_list.add(7)
 our_list.add(10)
-#our_list.remove(10)
-# our_list.remove_first()
-# our_list.remove_last()
-
+our_list.add(90)
+our_list.add(45)
+# our_list.remove_from_beginning()
+# our_list.remove_from_end()
+# our_list.remove_from_specific_position()
 our_list.display()
-#our_list.size()
+print("The reverse list is: " + str(our_list.reverse()))
+print("\n")
 print("The length of the list is " + str(our_list.size()))
